0187ec0dc0a2a0447bf07101b9277e197d68ed12
KulyginaJulia <linka805@gmail.com>
2017-03-04 18:13:07 +0300

    FirstCommit/ Filter - Invert

---
+++ Filters_kulygina/Filters_kulygina/Filters.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Drawing;
+using System.ComponentModel;
+
+namespace Filters_kulygina
+{
+
+    abstract class Filters
+    {
+        protected abstract Color calculateNewPixelColor(Bitmap sourceImage, int x, int y);
+
+        public Bitmap processImage(Bitmap sourceImage, BackgroundWorker worker)
+        {
+          Bitmap resultImage = new Bitmap(sourceImage.Width, sourceImage.Height);
+            
+          for (int i = 0; i < sourceImage.Width; i++)
+          {
+            worker.ReportProgress((int)((float)i / resultImage.Width * 100));
+            if (worker.CancellationPending)
+                return null;
+            for (int j = 0; j < sourceImage.Height; j++)
+            {
+                resultImage.SetPixel(i, j, calculateNewPixelColor(sourceImage, i, j));
+            }
+          }
+          return resultImage;
+         }
+
+        public int Clamp(int value, int min, int max)
+        {
+            if (value < min)
+                return min;
+            if (value > max)
+                return max;
+            return value;
+        }
+    }
+
+    class InvertFilter : Filters
+    {
+        public InvertFilter() { }
+        protected override Color calculateNewPixelColor(Bitmap sourceImage, int x, int y)
+        {
+            Color sourceColor = sourceImage.GetPixel(x, y);
+            Color resultColor = Color.FromArgb(255 - sourceColor.R, 255 - sourceColor.G, 255 - sourceColor.B);
+            return resultColor;
+        }
+
+
+
+    
+    }
+}
---
+++ Filters_kulygina/Filters_kulygina/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+//using System.ComponentModel.BackgroundWorker;
+
+namespace Filters_kulygina
+{
+    public partial class Form1 : Form
+    {
+        Bitmap image;
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void открытьИзображениеToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Image files | *.png; *.jpg; *.bmp; | All files (*.*) | *.* ";
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                image = new Bitmap(dialog.FileName);
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+
+            }
+        }
+
+        private void инверсияToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Filters filter = new InvertFilter();
+            //backgroundWorker1.RunWorkerAsync(filter);
+            Bitmap resultImage = filter.processImage(image, backgroundWorker1);
+            backgroundWorker1.RunWorkerAsync(filter);
+            pictureBox1.Image = resultImage;
+            pictureBox1.Refresh();
+            backgroundWorker1.Dispose();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Bitmap newImage = ((Filters)e.Argument).processImage(image, backgroundWorker1);
+            if (backgroundWorker1.CancellationPending != true)
+                image = newImage;
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = e.ProgressPercentage;
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if (!e.Cancelled)
+            {
+                pictureBox1.Image = image;
+                pictureBox1.Refresh();
+
+            }
+            progressBar1.Value = 0;
+        }
+
+        private void button_Cancel(object sender, EventArgs e)
+        {
+            backgroundWorker1.CancelAsync();
+        }
+
+
+    }
+}
